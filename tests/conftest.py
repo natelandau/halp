8cf3dee53396c55a4d62131c0a3e148890ffcd4e
Nathaniel Landau <nate@natelandau.com>
2025-03-02 17:09:18 -0500

    ci: fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -351,8 +351,7 @@ def mock_config(debug, tmp_path):
             "FILE_EXCLUDE_REGEX": "",
             "FILE_GLOBS": [],
             "CATEGORIES": {},
-            # "DB_PATH": tmp_path / "test.db",
-            # "DB_PATH": ":memory:",
+            "DB_PATH": ":memory:",
         }
 
         override_data = {key: value for key, value in kwargs.items() if value is not None}
